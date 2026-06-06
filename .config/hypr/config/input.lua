af0b2502c14ae8f3011d895714f79a9667ce03f5
Virean <gama.bruno.196@gmail.com>
2026-06-06 22:53:05 +0200

    removed single device settings

--- .config/hypr/config/input.lua
+++ .config/hypr/config/input.lua
@@ -1,30 +1,23 @@
 hl.config({
-    input = {
-        kb_layout  = "pt",
-        kb_variant = "",
-        kb_model   = "",
-        kb_options = "",
-        kb_rules   = "",
+	input = {
+		kb_layout = "pt",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
 
-        follow_mouse = 0,
+		follow_mouse = 0,
 
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-        force_no_accel = true, -- If true, overrides per-device accel settings and forces no acceleration on all devices.
-        touchpad = {
-            natural_scroll = false,
-        },
-    },
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+		force_no_accel = true, -- If true, overrides per-device accel settings and forces no acceleration on all devices.
+		touchpad = {
+			natural_scroll = false,
+		},
+	},
 })
 
 hl.gesture({
-    fingers = 3,
-    direction = "horizontal",
-    action = "workspace"
-})
-
--- Example per-device config
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
-hl.device({
-    name        = "epic-mouse-v1",
-    sensitivity = 0,
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
